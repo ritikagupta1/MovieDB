b84506df4b322b0c424fa2092c6fb6ee41504bae
Ritika Gupta <ritikagupta2020@gmail.com>
2024-10-26 12:32:13 +0530

    Initial Commit

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Ritika Gupta on 26/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
